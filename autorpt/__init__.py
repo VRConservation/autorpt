d6389e149aad887677fe7841d02ac260a29014ed
VRConservation <your-email@github.com>
2026-07-18 13:48:55 +0100

    Bump version: 1.0.0 → 1.1.0

--- autorpt/__init__.py
+++ autorpt/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Vance Russell"""
 __email__ = 'vance@3point.xyz'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 # Import main functionality for easy access
 from .autorpt import main
