9c806ab2e090e676139eb78dc0b1f811076e2030
VRConservation <your-email@github.com>
2026-07-18 15:04:08 +0100

    Bump version: 1.1.3 → 1.1.4, fix PyPI secrets inheritance

--- autorpt/__init__.py
+++ autorpt/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Vance Russell"""
 __email__ = 'vance@3point.xyz'
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 
 # Import main functionality for easy access
 from .autorpt import main
